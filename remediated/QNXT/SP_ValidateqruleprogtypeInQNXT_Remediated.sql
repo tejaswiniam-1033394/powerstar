a3296423bf231e6deecd28abb88eaa68ae509c90
tejaswiniam-1033394 <144168815+tejaswiniam-1033394@users.noreply.github.com>
2026-04-06 11:42:15 +0530

    Added SP_ValidateqruleprogtypeInQNXT_Remediated.sql file

--- remediated/QNXT/SP_ValidateqruleprogtypeInQNXT_Remediated.sql
+++ remediated/QNXT/SP_ValidateqruleprogtypeInQNXT_Remediated.sql
@@ -1,1 +1,10 @@
--- Remediated SQL content would be inserted here.
+USE [QNXT_MOLINADB_UNV];
+GO
+
+ALTER PROCEDURE [dbo].[SP_ValidateqruleprogtypeInQNXT]
+AS
+BEGIN
+    /* Remediation implemented to address schema change */
+    SELECT [Column1], [Column2], [Column3]
+    FROM QNXT_PLANDATA_UNV.dbo.qruleprogtype_replacement; 
+END
